3f7ba5d0cc93ee4e74c0d0ba134db93b154bf399
Abiaina <abinnet.a@gmail.com>
2018-05-31 15:50:36 -0700

    more styling attempted, created function to assign class to chat message, unable to change DOM

--- src/App.js
+++ src/App.js
@@ -5,43 +5,40 @@ import Message from './components/Message';
 
 
 class App extends Component {
-
+  isLocal = (author) => {
+    if(author === 'Vladimir'){
+      return 'local'
+    }
+    return 'remote'
+  }
 
   render() {
-    console.log(chatMessages);
-
-    const isLocal = (author) => {
-      if(author === 'Vladimir'){
-        return 'local'
-      }
-      return 'remote'
-    }
 
     const messageComponents = chatMessages.map((chatMessage, i=0) => {
       i += 1
       return (
         <section className="chat-entry">
-          <output className={isLocal(chatMessage.sender)}>
-              < Message
-                sender = {chatMessage.sender}
-                body = {chatMessage.body}
-                timeStamp = {chatMessage.timeStamp}
-              />
-          </output>
+        <output className={() => {this.isLocal(chatMessage.author)}}>
+        < Message
+        sender = {chatMessage.sender}
+        body = {chatMessage.body}
+        timeStamp = {chatMessage.timeStamp}
+        />
+        </output>
         </section>
       );
-  });
+    });
 
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Application title</h1>
-        </header>
-        <main className="App-main">
-          <div>
-            {messageComponents}
-          </div>
-        </main>
+      <header className="App-header">
+      <h1 className="App-title">Application title</h1>
+      </header>
+      <main className="App-main">
+      <div>
+      {messageComponents}
+      </div>
+      </main>
       </div>
     );
   }
